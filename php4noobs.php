01158029d23765c4739bed95df1669acb6b460d1
joaomoraski <joaomoraskilunkes@gmail.com>
2020-08-09 14:42:56 -0300

    first commit

---
+++ php4noobs.php
@@ -0,0 +1,111 @@
+<?php
+
+/*$listao = array(
+    chave1 => 10,
+    chave2 => 2,
+    chave3 => "Oiiii!",
+);
+
+var_dump($listao);
+
+$array1 = array(
+    "dev" => "danielhe4rt",
+    "group" => "he4rtdevs"
+);
+
+// Ou se você quiser um jeito mais simples de instanciar um array:
+
+$array2 = [
+    "dev" => "danielhe4rt",
+    "group" => "he4rtdevs"
+];
+
+var_dump($array1);
+var_dump($array2);
+*/
+/*
+ *
+ * // Aprendendo a concatenar stings e case-sensitive
+$Aprendendo = 'Eu ';
+$aPrendendo = 'Sou ';
+$apRendendo = 'O ';
+$aprEndendo = 'Moraski ';
+$apreNdendo = 'Amo PHP ';
+
+echo $Aprendendo . $aPrendendo . $apRendendo . $aprEndendo . $apreNdendo;
+*/
+/*
+ * switch case
+     * switch (cond) {
+    case val1:
+    // faça algo
+    break;
+    case val2:
+    // faça algo
+    break;
+    case val3:
+    // faça algo
+    break;
+    defaut:
+    // faça algo quando nenhuma das opções for selecionada
+    break;
+    }*/
+ /*
+  * If ternario
+  * $nickname = 'danielhe4rt';
+
+    $who = $nickname == "jorge123" ? "é o jorge online" : "não é o jorge online";
+
+    echo $who; //  não é o jorge online
+ */
+
+/*
+ * Repetição
+ * for( valor inicial; condição; incremento/decremento){}
+ * for($i = 0; $i < 10; $i++){}
+ *
+    for($i = 0; $i < 10; $i++){
+        echo "estudando php ". PHP_EOL;
+    }
+ * */
+
+
+/*
+ * ////For each
+ * */
+
+//echo '//Executando foreach'. PHP_EOL;
+//
+//$names = ["João","Vitor","Pedro"];
+//// Iteração sem indice
+//foreach ($names as $name){
+//    echo $name . " ";
+//}
+//
+//echo PHP_EOL;
+//// Iteração com indice
+//foreach ($names as $key => $name){
+//    echo $key . " = " . $name . " ";
+//}
+
+//
+//$pessoa = new StdClass;
+//
+//$pessoa->nome = "João Moraski";
+//$pessoa->idade = "18";
+//$pessoa->trabalho = "Estagiario fodido";
+//
+//foreach ($pessoa as $chave => $valor){
+//    echo "$chave: $valor". PHP_EOL;
+//}
+//
+
+
+
+//// Namespace = agrupar classes e tals que estão reelacionadas
+/// subnamespace = namespace taltal/taltal
+/// Pode usar o use para importar classes e etc..
+/// use tal/taltaltal
+///
+
+?>
